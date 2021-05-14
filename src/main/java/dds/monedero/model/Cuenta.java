4d862a2285f5af14f55329876a32435b45e492c0
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-05-14 18:33:56 -0300

    Saco Metodos que no se utilizan

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -18,13 +18,6 @@ public class Cuenta {
     saldo = 0;
   }
 
-  public Cuenta(double montoInicial) {
-    saldo = montoInicial;
-  }
-
-  public void setMovimientos(List<Movimiento> movimientos) {
-    this.movimientos = movimientos;
-  }
 
   public void poner(double cuanto) {
     esMontoNegativo(cuanto);
